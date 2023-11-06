7d726757eafc8fbd5b0a0e0e591a1cfad1a48324
crisyg97 <cristiangaray101@gmail.com>
2023-11-06 10:53:31 -0300

    ejercicios posible forma

--- vectores.asm
+++ vectores.asm
@@ -1,17 +1,40 @@
 section .data
 	vector dw 1,2,3,4
 	versor dw 1,0,0,0
-
+	valor dd 5
 
 section .text
-
-main:
-	movq mm0, qword[vector] ;carga del vector en un registro MMX
+global ejer1
+global ejer2
+global ejer3
+global ejer4
+ejer1:
+    mov ebp, esp; for correct debugging
+    movq mm0, qword[vector] ;carga del vector en un registro MMX
 	
-	movd eax, mm0
+    movd eax, mm0
 	
-	pmullw ;porque multipica?
-	
-	psrlq mm0, 16 ;realiza el desplazamiento hacia la derecha en valores empaquetados de 64bits
+    ;pmullw ;porque multipica?
+    movq mm2, 10
+    psrlq mm1, mm0, mm2;realiza el desplazamiento hacia la derecha en valores empaquetados de 64bits
 	;mueve hacia la derecha en 16 bits
-
+	
+ejer2:
+	paddw mm0,mm1
+	
+ejer3:
+	pxor mm0,mm0
+	
+ejer4:
+	movd mm0, dword [valor]
+	movd mm1, dword [vector]
+	pmulld mm1, mm0
+	movd mm2, dword [vector + 4]
+	pmulld mm2, mm0
+	;sumar los resultados parciales
+	paddd mm1, mm2
+	paddd mm1, mm3
+	paddd mm1, mm4
+	
+	
+	
--- vectores.c
+++ vectores.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
-
-
+//ejercicio2
+void SumarVectores (char *vectorA, char *vectorB, char *vectorResult, int dimension)
+//ejercicio3
+void InicializarVectorEnCero (char *vectorA, int dimension)
+//ejercicio4
+void productoEscalar (char *vector, int n)
 mai(){
 	
 	return 0;
